3d11bc059075e7934fba569e1fc25a8fa929db46
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-08-19 00:46:31 +0700

    Add Generator Code

--- util/util.go
+++ util/util.go
@@ -1,11 +1,22 @@
 package util
 
 import (
-    "math/rand"
+	cRand "crypto/rand"
+    mRand "math/rand"
     "time"
+    "fmt"
 )
 
 func Rand(min int, max int) int {
-    rand.Seed(time.Now().UnixNano())
-    return rand.Intn(max-min) + min
+    mRand.Seed(time.Now().UnixNano())
+    return mRand.Intn(max-min) + min
+}
+
+func GenerateCode(n int) {
+    b := make([]byte, n)
+    if _, err := cRand.Read(b); err != nil {
+        panic(err)
+    }
+    s := fmt.Sprintf("%X", b)
+    return s
 }
